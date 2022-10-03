ebf2ab932354eab25fbb9a84bea1384311e130b6
Sherlock Holmes <rajabzadehm43@gmail.com>
2022-10-03 13:25:41 +0330

    change module frmo es2015 to CommonJs

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,11 @@
-export const removeArrayDuplicates = (inputArray) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeArrayDuplicates = void 0;
+const removeArrayDuplicates = (inputArray) => {
     if (!Array.isArray(inputArray))
         inputArray = [];
     let theSet = new Set(inputArray);
     let uniqueArray = [...theSet];
     return uniqueArray;
 };
+exports.removeArrayDuplicates = removeArrayDuplicates;
